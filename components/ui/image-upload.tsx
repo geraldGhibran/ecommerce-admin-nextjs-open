1f1b0296c5b03ef4167d3439e4c993f6679311a0
geraldGhibran <gerald.gunasha@gmail.com>
2023-08-28 00:51:16 +0700

    first commit

--- components/ui/image-upload.tsx
+++ components/ui/image-upload.tsx
@@ -53,7 +53,7 @@ const ImageUpload: React.FC<ImageUploadProps> = ({
           </div>
         ))}
       </div>
-      <CldUploadWidget onUpload={onUpload} uploadPreset="t4drjppf">
+      <CldUploadWidget onUpload={onUpload} uploadPreset="ppw4o7ow">
         {({ open }) => {
           const onClick = () => {
             open();
